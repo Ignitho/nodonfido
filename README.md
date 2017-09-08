7807c8992de0454093d5f59b8313d35ba07893b1
vineeth raju <vineeth.raju@ignitho.com>
2017-09-08 12:06:57 +0530

    nodonfio package for Onfido verification

--- README.md
+++ README.md
@@ -1,2 +1,203 @@
-# nodonfido
-This is a complete example to Onfido API using JS.
+﻿
+	This is a complete example to Onfido API using JS.
+
+## Installing
+
+	npm install nodonfido
+
+## Usage
+
+	var nodonfido = require('nodonfido');	
+
+## Setup API Key
+
+	nodonfido.setAPIkey('My_API_Key');
+
+## Create an Applicant
+
+
+	var FormData = {
+    			'title': 'Mr',
+    			'first_name': 'first name',
+    			'last_name': 'last name',
+    			'gender': 'male',    
+    			'dob': '2013-02-17',
+    			'telephone': '322222222',
+    			'country': 'GBR',
+    			'email': 'name@email.com',
+    			'addresses': [{
+            			'building_number': '100',
+            			'street': 'Main Street',
+            			'town': 'London',
+            			'postcode': 'SW4 6EH',
+            			'country': 'GBR',
+            			'start_date': '2013-02-17'
+        			}]
+			}
+
+	nodonfido.Applicant('Create', FormData, '', function (error, response, body) {
+    		if (!error) {
+       		// Print out the response body
+        	console.log(body)
+    		} 
+    		else { console.log('Error : ' + error) }
+		});
+	
+
+## List all Applicants
+
+
+		var FormData = {};
+
+		nodonfido.Applicant('Fetch', FormData, '', function (error, response, body) {
+    			if (!error) {
+        			// Print out the response body
+        			console.log(body)
+    			} 
+    			else { console.log('Error : ' + error) }
+		});
+
+
+## Fetch Applicant by ID
+
+
+ 	--> Syntax
+ 		nodonfido.Applicant('Fetchid', FormData, Applicant_Id, function (error, response, body) {});
+
+
+	var FormData = {};
+
+	nodonfido.Applicant('Fetchid', FormData, 'e4ertf7-3345-4efe-bb27-d193452428c88', function (error, response, body) {
+    		if (!error) {
+        	// Print out the response body
+        	console.log(body)
+    		} 
+    		else { console.log('Error : ' + error) }
+	});
+
+
+## Upload Document for an Applicant
+
+
+ 	--> Syntax
+		nodonfido.Document( applicant_id, file_name, file_type,doc_type,file_location, function (error, response, 	
+
+	body) {});
+
+
+		nodonfido.Document('e4ertf7-3345-4efe-bb27-d193452428c88', 'image.jpg', 'jpg', 'passport', 'C:\\image.jpg', 
+
+ 
+		function (error, response, body) {
+    			if (!error) {
+        		// Print out the response body
+        		console.log(body)
+    		} 
+    		else { console.log('Error : ' + error) }
+		});
+
+
+## Create Checks for an Applicant
+
+
+	--> Syntax
+		nodonfido.Checks('Create', check_args, applicant_id, ' ', function (error, response, body){});
+
+
+			check_args = {
+    				type: 'standard',
+    				reports: [{ name: 'identity' }]
+				}
+
+			nodonfido.Checks('Create', check_args, 'e4ertf7-3345-4efe-bb27-d193452428c88', ' ', function 		
+
+			(error, response, body) {
+   			 if (!error) {
+    	   			 // Print out the response body
+       				 console.log(body)
+   			 } 
+  			  else { console.log('Error : ' + error) }
+			});
+
+
+## Fetch All Checks for an Applicant
+
+
+	--> Syntax
+		nodonfido.Checks('Fetch', check_args, applicant_id, ' ', function (error, response, body){});
+
+
+		check_args = { }
+
+		nodonfido.Checks('Fetch', check_args, 'e4ertf7-3345-4efe-bb27-d193452428c88', '', function (error, 		
+
+		response, body) {
+    			if (!error) {
+        			// Print out the response body
+        			console.log(body)
+    			} 
+    			else { console.log('Error : ' + error) }
+		});
+
+
+## Fetch Checks by CheckId
+
+
+	--> Syntax
+		nodonfido.Checks('Fetchid', check_args, applicant_id,check_id, function (error, response, body){});
+
+
+		check_args = { } 
+
+		nodonfido.Checks('Fetchid', check_args, 'e4ertf7-3345-4efe-bb27-d193452428c88', '74dfgh42c-cc38-48aa-8gh4-	
+
+		e2cfghfg21b30', function 
+		(error, response, body) {
+    			if (!error) {
+        		// Print out the response body
+        		console.log(body)
+    		} 
+    		else { console.log('Error : ' + error) }
+		});
+
+
+## List Reports for an applicant by CheckID
+
+
+	--> Syntax
+		nodonfido.Reports('Fetch',FormData, check_id,' ', function (error, response, body){});
+
+
+		var FormData = {};
+
+			nodonfido.Reports('Fetch', FormData, '74dfgh42c-cc38-48aa-8gh4-e2cfghfg21b30', '', function (error, 
+
+			response, body) {
+    				if (!error) {
+        				// Print out the response body
+        				console.log(body)
+    				} 
+    				else { console.log('Error : ' + error) }
+			});
+
+
+## Fetch Reports by ReportID
+
+
+	--> Syntax
+		nodonfido.Reports('Fetchid',FormData, check_id,report_id, function (error, response, body){});
+
+
+		var FormData = {};
+
+		nodonfido.Reports('Fetchid', FormData, '74dfgh42c-cc38-48aa-8gh4-e2cfghfg21b30', '0bfhj597-fec5-4f22-b9a7-	
+
+	28ab0fgh1837', 
+   			 function(error, response, body) {
+   				 if (!error) {
+       					 // Print out the response body
+       					 console.log(body)
+    				} 
+    				else { console.log('Error : ' + error) }
+			});
+
---
+++ index.js
@@ -0,0 +1,319 @@
+﻿var request = require('request');
+var fs = require('fs');
+
+
+var Nodofido = function (url, method, api_key, host, path, formdata) {
+    
+    
+    var options = {
+        url: url,   
+        method: method,
+        headers: { 'Authorization': 'Token token=' + api_key, 'host': host, 'path': path, },        
+        body: formdata,
+        json: true
+    }
+    
+    
+    request(options, function (error, response, body) {
+        console.log("errrrr: " + error);
+        console.log("statusCode: " + response.statusCode); if (!error && response.statusCode == 201) {
+            // Print out the response body
+            console.log(body)
+            console.log("BODYertett: " + body);
+            var result = {};
+            result.response = response;
+            result.body = body;
+            result.error = error;
+            
+            
+            response.send(result);
+        }
+    })
+
+}
+
+var API_KEY = '';
+
+//Set API Key for connect Onfido
+//--------------------------------------
+Nodofido.setAPIkey = function (api_key) {
+    API_KEY = api_key;
+}
+
+//Applicant
+//--------------------------------------
+Nodofido.Applicant = function (opration,formdata, applicant_id, callback) {
+    
+    var options = {}
+    
+
+    if (opration == 'Create') {
+
+        var url = 'https://api.onfido.com/v2/applicants';
+        var path = '/v2/applicants';                
+
+        options = {
+            url: url,   
+            method: 'POST',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Fetch') {
+        var url = 'https://api.onfido.com/v2/applicants';
+        var path = '/v2/applicants';
+                
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Edit') {
+        var url = 'https://api.onfido.com/v2/applicants';
+        var path = '/v2/applicants';
+        
+        if (applicant_id != '') {
+            url = url + '/' + applicant_id;
+            path = path + '/' + applicant_id;
+        }
+        
+        options = {
+            url: url,   
+            method: 'PUT',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Fetchid') {
+        var url = 'https://api.onfido.com/v2/applicants';
+        var path = '/v2/applicants';
+        
+        if (applicant_id != '') {
+            url = url + '/' + applicant_id;
+            path = path + '/' + applicant_id;
+        }
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else {
+        return callback('Error:Invalid opration', '', '');
+    }
+
+
+    
+    request(options, function (error, response, body) {
+        //console.log("errrrr: " + error);
+        //console.log("statusCode: " + response.statusCode);
+        if (!body.error) {
+            // Print out the response body
+            //console.log(body)
+            //console.log("BODYertett: " + body);
+            var result = {};
+            result.response = response;
+            result.body = body;
+            result.error = error;
+            
+            return callback(error, response, body);
+            //response.send(result);
+            module.exports = result;
+        }
+        else {
+            var result = {};
+            result.error = body.error;
+            return callback(error, response, body);
+        }
+    })
+
+}
+
+//Document Upload
+//--------------------------------------
+Nodofido.Document = function (applicant_id,file_name,file_type,doc_type,file_location,callback) { 
+
+    var formData = {
+        file_name: file_name,
+        file_type: file_type,
+        type: doc_type,
+        file: fs.createReadStream(file_location)
+    };
+    
+    var url = 'https://api.onfido.com/v2/applicants/' + applicant_id+'/documents';
+
+    request.post({
+        url: url, 
+        formData: formData, 
+        headers: { 'Authorization': 'Token token='+ API_KEY }
+    }, function (err, httpResponse, body) {
+        if (err) {
+            return callback('Error:Upload Failed', '', '');
+        }
+        return callback(err, httpResponse, body);
+    });
+
+}
+
+//Checks
+//--------------------------------------
+Nodofido.Checks = function (opration, formdata, applicant_id,check_id, callback) {
+    
+    var options = {}
+    
+    
+    if (opration == 'Create') {
+        
+        var url = 'https://api.onfido.com/v2/applicants/'+ applicant_id+'/checks';
+        var path = '/v2/applicants/'+ applicant_id+'/checks';
+        
+        options = {
+            url: url,   
+            method: 'POST',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Fetch') {
+        var url = 'https://api.onfido.com/v2/applicants/' + applicant_id + '/checks';
+        var path = '/v2/applicants/' + applicant_id + '/checks';
+        
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Fetchid') {
+        var url = 'https://api.onfido.com/v2/applicants/' + applicant_id + '/checks';
+        var path = '/v2/applicants/' + applicant_id + '/checks';
+        
+        if (check_id != '') {
+            url = url + '/' + check_id;
+            path = path + '/' + check_id;
+        }
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else {
+        return callback('Error:Invalid opration', '', '');
+    }
+    
+    
+    
+    request(options, function (error, response, body) {
+        //console.log("errrrr: " + error);
+        //console.log("statusCode: " + response.statusCode);
+        if (!body.error) {
+            // Print out the response body
+            //console.log(body)
+            //console.log("BODYertett: " + body);
+            var result = {};
+            result.response = response;
+            result.body = body;
+            result.error = error;
+            
+            return callback(error, response, body);
+            //response.send(result);
+            module.exports = result;
+        }
+        else {
+            var result = {};
+            result.error = body.error;
+            return callback(error, response, body);
+        }
+    })
+
+}
+
+//Reports
+//--------------------------------------
+Nodofido.Reports = function (opration, formdata, check_id,report_id, callback) {
+    
+    var options = {}
+    
+    
+    if (opration == 'Fetch') {
+        var url = 'https://api.onfido.com/v2/checks/' + check_id + '/reports';
+        var path = '/v2/checks/' + check_id + '/reports';
+        
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else if (opration == 'Fetchid') {
+        var url = 'https://api.onfido.com/v2/checks/' + check_id + '/reports';
+        var path = '/v2/checks/' + check_id + '/reports';
+        
+        if (report_id != '') {
+            url = url + '/' + report_id;
+            path = path + '/' + report_id;
+        }
+        
+        options = {
+            url: url,   
+            method: 'GET',
+            headers: { 'Authorization': 'Token token=' + API_KEY, 'host': 'api.onfido.com', 'path': path, },        
+            body: formdata,
+            json: true
+        }
+    }
+    else {
+        return callback('Error:Invalid opration', '', '');
+    }
+    
+    
+    
+    request(options, function (error, response, body) {
+        //console.log("errrrr: " + error);
+        //console.log("statusCode: " + response.statusCode);
+        if (!body.error) {
+            // Print out the response body
+            //console.log(body)
+            //console.log("BODYertett: " + body);
+            var result = {};
+            result.response = response;
+            result.body = body;
+            result.error = error;
+            
+            return callback(error, response, body);
+            //response.send(result);
+            module.exports = result;
+        }
+        else {
+            var result = {};
+            result.error = body.error;
+            return callback(error, response, body);
+        }
+    })
+
+}
+
+
+module.exports = Nodofido;
+
+
